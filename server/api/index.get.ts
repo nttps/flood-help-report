3077bdd6290928c2944b0a11b74a593387346b34
NTTPS <teamerze@gmail.com>
2024-10-01 18:20:25 +0700

    add page

---
+++ server/api/index.get.ts
@@ -0,0 +1,121 @@
+import sql from 'mssql';
+
+const config = {
+  user: 'dalert',
+  password: '@min#DSS',
+  database: 'DPM_HELP67',
+  server: '122.154.29.20',
+  options: {
+    encrypt: true, // ถ้าเชื่อมต่อแบบ SSL
+    trustServerCertificate: true, // ถ้าไม่ใช้ SSL
+  }
+};
+
+const getHeader = async (sql: typeof import('mssql')) => {
+  await sql.connect(config);
+  const result = await sql.query(`
+    WITH counts AS (
+    SELECT 
+      rq.p_no,  -- จังหวัด
+      rq.p_name,  -- จังหวัด
+      COUNT(ch.person_qty) AS person_qty,  -- จำนวน ก.ช.ภ.จ
+      COUNT(DISTINCT cl.commit_id) AS send,  -- ส่งปกครอง
+      SUM(CASE WHEN cl.linkgate_status != 'ปกติ' THEN 1 ELSE 0 END) AS failed_linkage,  -- ไม่ผ่าน Linkage
+      COUNT(cl.payment_status) AS send_bank,  -- ส่งออมสิน
+      COUNT(DISTINCT cl.commit_id) AS count_total_commit,  -- จำนวนประชุมทั้งหมด / ครั้ง
+      SUM(CASE WHEN cl.payment_status = 'สำเร็จ' THEN 1 ELSE 0 END) AS successful_payments,  -- โอนสำเร็จ
+      SUM(CASE WHEN cl.payment_status = 'ปฏิเสธ' THEN 1 ELSE 0 END) AS unsuccessful_payments,  -- โอนไม่สำเร็จ
+      (SELECT COUNT(*) 
+        FROM sf_commit_head cc 
+        WHERE step_id = 'จังหวัด' 
+        AND commit_type = 'ตีกลับ' 
+        AND rq.p_no = cc.pcode) AS count_back_to_province,  -- นับจำนวนจังหวัดที่ยังรอดำเนินการ
+      (SELECT COUNT(*) 
+        FROM sf_commit_head cc 
+        WHERE step_id = 'จังหวัด' 
+        AND commit_type = 'ตีกลับ' 
+        AND status_confirm = 'รอยืนยัน' 
+        AND rq.p_no = cc.pcode) AS outstanding  -- คงค้าง
+    FROM sf_commit_line cl
+    LEFT JOIN sf_commit_head ch ON ch.commit_id = cl.commit_id
+    LEFT JOIN sf_help_request rq ON rq.req_id = cl.req_id
+    WHERE ch.step_id = 'ปภ.' 
+    AND cl.is_active = 1
+    GROUP BY rq.p_name, rq.p_no
+    )
+
+    SELECT *,
+    0 AS retreat,
+        count_back_to_province - outstanding AS send_from_province  -- จังหวัดส่งคืนผลตรวจสอบ (calculated as count_back_to_province - outstanding)
+    FROM counts;
+    `); 
+
+  return result.recordset;
+}
+
+
+const getSub = async (sql: typeof import('mssql'), p_no: number) => {
+  await sql.connect(config);
+  const result = await sql.query(`
+    WITH sub_counts AS (
+
+    SELECT 
+      rq.p_no,
+      rq.p_name,
+        ch.commit_no, -- จังหวัด
+        ch.commit_date, -- จังหวัด
+        COUNT(ch.person_qty) AS person_qty, -- จำนวน ก.ช.ภ.จ
+        COUNT(DISTINCT cl.commit_id) AS send,  -- ส่งปกครอง
+        SUM(CASE WHEN cl.linkgate_status != 'ปกติ' THEN 1 ELSE 0 END) AS failed_linkage, --ไม่ผ่าน Linkage
+        COUNT(cl.payment_status) AS send_bank, --- ส่งออมสิน
+        MAX(cl.payment_date) AS latest_payment_date, -- วันที่โอนเงินล่าสุด
+        COUNT(DISTINCT cl.commit_id) AS count_total_commit,  -- จำนวนประชุมทั้งหมด / ครั้ง
+        SUM(CASE WHEN cl.payment_status = 'สำเร็จ' THEN 1 ELSE 0 END) AS successful_payments,  --โอนสำเร็จ
+        SUM(CASE WHEN cl.payment_status != 'สำเร็จ' THEN 1 ELSE 0 END) AS unsuccessful_payments,  -- โอนไม่สำเร็จ
+        (SELECT COUNT(*) 
+          FROM sf_commit_head cc 
+          WHERE step_id = 'จังหวัด' 
+          AND commit_type = 'ตีกลับ' 
+          AND rq.p_no = cc.pcode) AS count_back_to_province,  -- นับจำนวนจังหวัดที่ยังรอดำเนินการ
+        (SELECT COUNT(*) 
+          FROM sf_commit_head cc 
+          WHERE step_id = 'จังหวัด' 
+          AND commit_type = 'ตีกลับ' 
+          AND status_confirm = 'รอยืนยัน' 
+          AND rq.p_no = cc.pcode) AS outstanding  -- คงค้าง
+    FROM sf_commit_head ch
+    LEFT JOIN sf_commit_line cl ON ch.commit_id = cl.commit_id
+    LEFT JOIN sf_help_request rq ON rq.req_id = cl.req_id
+    WHERE ch.step_id = 'ปภ.' AND cl.is_active = 1 AND rq.p_no = '${p_no}'
+    GROUP BY rq.p_no, rq.p_name,  ch.commit_date, ch.commit_no
+    )
+    SELECT *,
+    0 AS retreat,
+          count_back_to_province - outstanding AS send_from_province  -- จังหวัดส่งคืนผลตรวจสอบ (calculated as count_back_to_province - outstanding)
+    FROM sub_counts;
+    `); 
+  return result.recordset;
+}
+
+
+export default defineEventHandler(async (event) => {
+  try {
+   
+    const results = []
+    const resultHead = await getHeader(sql);
+
+    // Loop ผ่านแต่ละ Head และเพิ่ม Sub เข้าไปในแต่ละ Head
+    for (const head of resultHead) {
+      const sub = await getSub(sql, head.p_no); // รับข้อมูล Sub โดยใช้ head.p_no เป็นตัวกำหนด
+
+      // เพิ่ม sub เข้าไปใน object head โดยตรง
+      head.sub = sub;
+
+      // เพิ่ม head (ที่มี sub) เข้าไปใน results
+      results.push(head);
+    }
+    return results;
+  } catch (err) {
+    console.error(err);
+  }
+})
