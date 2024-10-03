3d2fd8366cd327e20995bff7d8f4f3a9d6d6289b
NTTPS <teamerze@gmail.com>
2024-10-03 23:41:07 +0700

    update

--- pages/onepage.vue
+++ pages/onepage.vue
@@ -1,68 +1,71 @@
 <template>
-    <div class="font-kanit w-[980px] mx-auto bg-[#F1F1F1] relative bg-[url('~/assets/images/bg-bottom.jpg')] bg-bottom bg-no-repeat bg-cover" ref="htmlContent">
+    <div class="font-kanit lg:w-[980px] mx-auto bg-[#F1F1F1] relative bg-[url('~/assets/images/bg-bottom.jpg')] bg-bottom bg-no-repeat bg-cover" ref="htmlContent">
         <!-- <img src="~/assets/images/bg-bottom.jpg" class="absolute bottom-0 w-full left-0 right-0" alt=""> -->
-        <header class="flex bg-primary px-10 py-2 space-x-8 justify-between items-center text-white rounded-b-3xl">
-            <div>
-                <img src="~/assets/images/logo.png" class="h-[140px]" alt="">
+        <header class="flex bg-primary px-5 lg:px-10 py-2 space-x-4 lg:space-x-8 justify-between items-center text-white rounded-b-3xl">
+            <div class=" min-w-max">
+                <img src="~/assets/images/logo.png" class="h-[100px] lg:h-[140px] min-w-max" alt="">
             </div>
-            <div class="text-2xl pt-6">
-                <h3 class="text-center mb-6">จำนวนคำร้องขอรับเงินช่วยเหลือผู้ประสบภัยในช่วงฤดูฝน ปี พ.ศ.2567</h3>
+            <div class="text-lg lg:text-2xl lg:pt-6">
+                <h3 class="text-center mb-4 lg:mb-6">จำนวนคำร้องขอรับเงินช่วยเหลือผู้ประสบภัยในช่วงฤดูฝน ปี พ.ศ.2567</h3>
                 <div class="flex justify-between">
-                    <div>ในระดับจังหวัด</div>
-                    <div class="text-lg font-[300] text-right">
-                        <div class="mb-1 text-white">ตามมติคณะรัฐมนตรี 17 กันยายน 2567</div>
-                        <div class="rounded-full bg-[#FFB800] inline-flex items-center py-1 px-4 text-[#051445]">
-                            <div class="mr-2">ณ วันที {{ format(new Date(), 'dd/MM/yyyy', { locale: th }).replace((new Date().getFullYear()).toString(), (new Date().getFullYear() + 543).toString()) }}</div>
-                            <UIcon name="disaster-clock" class="mr-2 text-[#051445]" />
-                            <div>
-                                09:00 น.
-                            </div>
+                    <div class="text-center lg:text-left">ในระดับจังหวัด</div>
+                    <div class="text-lg font-[300] text-center lg:text-right text-white mb-1">
+                        ตามมติคณะรัฐมนตรี 17 กันยายน 2567
+                    </div>
+                </div>
+                <div class="flex justify-end text-base lg:text-lg">
+                    <div class="rounded-full ml-a bg-[#FFB800] inline-flex items-center py-1 px-4 text-[#051445]">
+                        <div class="mr-2">ณ วันที {{ format(new Date(), 'dd/MM/yyyy', { locale: th }).replace((new Date().getFullYear()).toString(), (new Date().getFullYear() + 543).toString()) }}</div>
+                        <UIcon name="disaster-clock" class="mr-2 text-[#051445]" />
+                        <div>
+                            09:00 น.
                         </div>
                     </div>
                 </div>
             </div>
         </header>
 
-        <main class="px-8 mt-2" v-if="pending">
+        <main class="px-4 lg:px-8 mt-2" v-if="pending">
             <section class="bg-primary rounded-2xl p-3">
-               <div class="flex gap-2">
-                    <div class="bg-secondary py-4 rounded-xl w-3/5 flex flex-col justify-center items-center">
+               <div class="flex flex-wrap lg:flex-nowrap gap-4 lg:gap-2">
+                    <div class="bg-secondary py-4 rounded-xl w-full lg:w-3/5 flex flex-col justify-center items-center">
                         <div class="font-semibold text-2xl mb-2">จำนวนคำร้องที่ขอรับการช่วยเหลือ</div>
                         <div class="text-white text-lg">{{ format(new Date(), 'dd MMMM yyyy', { locale: th }).replace((new Date().getFullYear()).toString(), (new Date().getFullYear() + 543).toString()) }} (ในแต่ละจังหวัด)</div>
                         <div class="text-white text-lg">คำร้องทั้งหมด <span class="text-4xl">{{ report.countRequest.toLocaleString() }}</span> คำร้อง  </div>
                     </div>
-                    <div class="bg-secondary rounded-xl w-2/5 flex flex-col justify-center items-center">
+                    <div class="bg-secondary rounded-xl w-full lg:w-2/5 flex flex-col justify-center items-center">
                         <div class="font-semibold text-2xl mb-2">จำนวนคำร้องสูงสุด</div>
                         <div class="text-2xl font-semibold mb-2">{{ report.topRequest.p_name }}</div>
                         <div class="text-2xl">{{ report.topRequest?.top_count?.toLocaleString() }}</div>
                     </div>
                </div>
-               <div class="flex text-primary my-5 items-center">
-                    <div class="w-1/3 text-center">
+               <div class="flex flex-wrap lg:flex-nowrap text-primary mt-5 items-center">
+                    <div class="w-full mb-4 lg:w-1/3 text-center">
                         <div>ครัวเรือนที่ได้รับเงินแล้ว</div>
                         <div><span class="text-3xl">{{ report.allTransfer?.toLocaleString() }}</span> ครัวเรือน</div>
                     </div>
-                    <div class="w-1/3 text-center">
+                    <div class="w-full mb-4 lg:w-1/3 text-center">
                         <div>จำนวนเงินที่โอนแล้ว</div>
                         <div><span class="text-3xl">{{ report.allMoneyTransfer?.toLocaleString() }}</span> บาท</div>
                     </div>
-                    <div class="w-1/3 text-center">
-                        <div>จังหวัดที่ได้รับเงินแล้ว</div>
-                        <div>/ จังหวัดที่กรอกคำร้อง</div>
+                    <div class="w-full mb-4 lg:w-1/3 text-center">
+                        <div>จังหวัดที่ได้รับเงินแล้ว <br class="hidden lg:block" />/ จังหวัดที่กรอกคำร้อง</div>
                         <div class="text-3xl">{{report.provinceRetrieveMoney?.toLocaleString() }}/{{ report.provinceRequest?.toLocaleString() }}</div>
 
                     </div>
                </div>
-               <div class="grid grid-cols-5 gap-2">
-                    <div class="bg-[#FFE196] rounded-xl p-4 text-center text-lg" v-for="d in report.allRequest.slice(1, 6)" :key="d">
-                        <div>{{d.p_name}}</div>
-                        <div>{{d.top_count.toLocaleString()}}</div>
+               <div class="lg:grid flex flex-wrap lg:grid-cols-5 lg:gap-2 justify-center ">
+                    <div class="w-1/3 p-1 lg:p-0 lg:w-auto" v-for="d in report.allRequest.slice(1, 6)" :key="d">
+                        <div class="bg-[#FFE196] rounded-xl lg:p-4 text-center text-lg " >
+                            <div>{{d.p_name}}</div>
+                            <div>{{d.top_count.toLocaleString()}}</div>
+                        </div>
                     </div>
                </div>
             </section>
-            <section class="rounded-2xl bg-white/10 backdrop-blur-3xl px-8 py-4">
-                <div class="grid grid-cols-5 gap-2 items-center">
-                    <div class="mb-5 text-white" v-for="a in report.allRequest.slice(6)" :key="a">
+            <section class="rounded-2xl bg-white/10 backdrop-blur-3xl px-2 lg:px-8 py-4">
+                <div class="grid grid-cols-3 lg:grid-cols-5 lg:gap-2 lg:items-center">
+                    <div class="mb-5 text-white break-words" v-for="a in report.allRequest.slice(6)" :key="a">
                         {{ `${a.p_name} (${a.top_count.toLocaleString()})` }}
                     </div>
                </div>
@@ -71,16 +74,16 @@
         <!-- Show loading spinner while data is loading -->
         
         <footer>
-            <div class="flex bg-primary px-10 py-4 space-x-8 justify-between items-center text-white rounded-t-3xl">
+            <div class="flex flex-wrap lg:flex-nowrap bg-primary px-5 lg:px-10 py-4 lg:space-x-8 justify-between items-center text-white rounded-t-3xl">
                 <div class="text-lg pt-4 pb-2">
-                    <div class="mb-2">ศูนย์เทคโนโลยีสารสนเทศและการสื่อสาร กรมป้องกันและบรรเทาสาธารณภัย</div>
-                    <div class="mb-4">ตรวจสอบสถานะคำร้องตนเองได้ที่ : <span class="font-semibold"><a target="_blank" href="https://flood67.disaster.go.th">https://flood67.disaster.go.th</a></span></div>
-                    <div class="flex justify-between items-center">
-                        <a href="https://www.facebook.com/DDPMNews" target="_blank" class="flex items-center">
+                    <div class="mb-2 text-center lg:text-left">ศูนย์เทคโนโลยีสารสนเทศและการสื่อสาร กรมป้องกันและบรรเทาสาธารณภัย</div>
+                    <div class="mb-4 text-center lg:text-left">ตรวจสอบสถานะคำร้องตนเองได้ที่ : <span class="font-semibold"><a target="_blank" href="https://flood67.disaster.go.th">https://flood67.disaster.go.th</a></span></div>
+                    <div class="flex flex-wrap justify-between items-center">
+                        <a href="https://www.facebook.com/DDPMNews" target="_blank" class="flex items-center  mb-2">
                             <UIcon name="disaster-facebook" size="40" />
                             <div class="ml-2">: กรมป้องกันและบรรเทาสาธารณภัย DDPM </div>
                         </a>
-                        <a href="https://line.me/R/ti/p/@1784DDPM" target="_blank" class="flex items-center">
+                        <a href="https://line.me/R/ti/p/@1784DDPM" target="_blank" class="flex items-center ">
                             <UIcon name="disaster-line" size="40" />
                             <div class="ml-2">: @1784DDPM  </div>
                         </a>
