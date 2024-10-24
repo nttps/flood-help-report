0f56345d78f5fc826a2ffa60cdbc203a35e1bd38
NTTPS <teamerze@gmail.com>
2024-10-24 13:21:00 +0700

    fix color

--- pages/onepage.vue
+++ pages/onepage.vue
@@ -60,7 +60,7 @@
                         <div class="p-1 lg:p-0 lg:w-auto" v-for="d in report.allRequest.slice(3, 7)" :key="d">
                             <div class="bg-white rounded-xl py-2 lg:p-4 text-center text-lg" >
                                 <div class="mb-4 text-xl">{{d.p_name}}</div>
-                                <div>{{d.top_count.toLocaleString()}} <span class="font-bold">(<span :class="{ 'text-green-300': d.total > 0}">{{d.total.toLocaleString()}}</span>)</span></div>
+                                <div>{{d.top_count.toLocaleString()}} <span class="font-bold">(<span :class="{ 'text-green-500': d.total > 0}">{{d.total.toLocaleString()}}</span>)</span></div>
                             </div>
                         </div>
                 </div>
@@ -68,7 +68,7 @@
                 <section class="rounded-2xl bg-white/10 backdrop-blur-3xl px-2 lg:px-8 py-4">
                     <div class="grid grid-cols-3 lg:grid-cols-5 lg:gap-2 lg:items-center">
                         <div class="mb-5 text-white break-words lg:py-0" v-for="a in report.allRequest.slice(7)" :key="a">
-                            {{ a.p_name }}({{ a.top_count.toLocaleString() }}/<span :class="{ 'text-green-300': a.total > 0}">{{ a.total.toLocaleString() }}</span>)
+                            {{ a.p_name }}({{ a.top_count.toLocaleString() }}/<span :class="{ 'text-green-500': a.total > 0}">{{ a.total.toLocaleString() }}</span>)
                         </div>
                 </div>
                 </section>
